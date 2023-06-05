97cced6506c0f94d479317f8f38dc3eedc30306d
Ronaldo <ronaldoballa99@gmail.com>
2023-06-05 12:43:35 +0200

    ff
    add

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>OG Tech PC - Landing Page</title>
+  <title>ARB - Tech PC - Landing Page</title>
   <?php 
     require "header.php"; 
     require_once "includes/class_autoloader.php";
@@ -27,7 +27,7 @@
       <li>
         <img src="./static/images/category_3.jpg"> 
         <div class="caption center-align">
-          <h3 class="bold page-title">At OG Tech</h3>
+          <h3 class="bold page-title">At ARB - Tech</h3>
           <h5 class="bold page-title">From pc to peripherals, we got u covered.</h5>
         </div>
       </li>
@@ -88,7 +88,7 @@
     <div class="wide-container">
       <h3 class="white-text center">BUILT BY ENTHUSIASTS FOR ENTHUSIASTS</h3>
       <h5 class="white-text center">
-        At <b class="orange-text">OG Tech PC</b>, We are a team of serious gamers and overclockers with a passion towards customized and fast PCs.
+        At <b class="orange-text">ARB - Tech PC</b>, We are a team of serious gamers and overclockers with a passion towards customized and fast PCs.
       </h5>
     </div>
   </div>
@@ -112,7 +112,7 @@
     </div>
   </div>
 
-  <h3 class="white-text center">OG Tech PC - White PC Build</h3>
+  <h3 class="white-text center">ARB - Tech PC - White PC Build</h3>
   <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" style="margin-bottom: 100px">
     <img src="static/images/ice_pc.png" style="cursor:pointer; display:block; margin: 0 auto; " />
   </div>
